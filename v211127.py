5b1af156b1a1cb0d778ede64290cbc076ec403c2
nkzt <masaki.nakazato@ves.co.jp>
2021-11-27 22:40:00 +0900

    read_csv / to_xlsx / read_xlsx

--- v211127.py
+++ v211127.py
@@ -1,10 +1,22 @@
 #! /usr/bin/env python3
 
 import sys
+import pandas as pd
 
-# main4
 def main():
-  print(sys.version)
+  # print(sys.version)
+
+  params = ', '.join(sys.argv)
+  print(params)
+
+  # df = pd.read_csv('./data.csv', header=None)
+  df = pd.read_excel('./data.xlsx', header=None)
+
+  print(df)
+  print(df[0][0])
+  print(df[1][2])
+
+  # df.to_excel('data.xlsx', index=False, header=False)
 
 if __name__ == '__main__':
   main()
