7e00f43d2dcb9fe3af15db18c869f6a537e10e38
nkzt <masaki.nakazato@ves.co.jp>
2021-11-27 23:00:22 +0900

    コメント追記

--- v211127.py
+++ v211127.py
@@ -9,14 +9,19 @@ def main():
   params = ', '.join(sys.argv)
   print(params)
 
+  # header=None, index_col=None
+  #   特定の行、列をヘッダー、インデックスとしない(0からの連番)
+  #   0〜 で特定の特定の行、列を指定
   df = pd.read_csv('./data.csv', header=None)
   # df = pd.read_excel('./data.xlsx', header=None)
 
   print(df)
-  print(df[0][0])
-  print(df[1][2])
+  print(df[0][0])   # a1
+  print(df[1][2])   # b3
 
-  # df.to_excel('data.xlsx', index=False, header=False)
+  # index=False, columns=False
+  #   index(行名)、columns(列名)を書き出さない
+  #   df.to_excel('data.xlsx', index=False, header=False)
 
 if __name__ == '__main__':
   main()
